dfeaf2776c6ff48c954961c5f150c5bab26e6027
phatdroid94 <phatdroid94@gmail.com>
2019-10-14 19:02:18 +0700

    push string , long

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/firebasedatabase/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/firebasedatabase/MainActivity.java
@@ -1,10 +1,24 @@
 package phamtanphat.ptp.khoaphamtraining.firebasedatabase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference mMyRef = mDatabase.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,8 +26,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        node : Cha => phai co 1 dua con
 //        child : con => phai co key va value
+        // Gui du lieu lieu
+        // 1 : String
+//        giangvien : Pham tan phat
+//        mMyRef.child("giangvien").setValue("pham tan phat").addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                if (task.isSuccessful()){
+//                    Toast.makeText(MainActivity.this, "Thanh cong", Toast.LENGTH_SHORT).show();
+//                }else{
+//                    Log.d("BBB",task.getException().toString());
+//                }
+//            }
+//        });
+        // 2 : Long
+//       khaigiang : thoidiemhientai
+//        mMyRef.child("khaigiang").setValue(Calendar.getInstance().getTimeInMillis()).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                if (task.isSuccessful()){
+//                    Toast.makeText(MainActivity.this, "Thanh cong", Toast.LENGTH_SHORT).show();
+//                }else{
+//                    Log.d("BBB",task.getException().toString());
+//                }
+//            }
+//        });
+        long currentTime = Calendar.getInstance().getTimeInMillis();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-YYYY");
+        Log.d("BBB",simpleDateFormat.format(currentTime));
     }
-
-
 
 }
