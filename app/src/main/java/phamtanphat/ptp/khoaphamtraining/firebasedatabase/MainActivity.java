fac78c092c3ee7fcb0a7d431db776cbba93d9946
phatdroid94 <phatdroid94@gmail.com>
2019-10-14 18:37:12 +0700

    init

---
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/firebasedatabase/MainActivity.java
@@ -0,0 +1,19 @@
+package phamtanphat.ptp.khoaphamtraining.firebasedatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        node : Cha => phai co 1 dua con
+//        child : con => phai co key va value
+    }
+
+
+
+}
